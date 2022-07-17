4b915513b08bf2d88018664d622c3fae5f53355d
Anise-002 <n-u-002@naver.com>
2022-07-17 23:13:06 +0900

    백준 1065

--- 백준문제풀기/41. 백준4673.js
+++ 백준문제풀기/41. 백준4673.js
@@ -1,28 +1,28 @@
 const n = 1000;
 
-function d(n){
-    let num = n;
-    let result = 0;
+function d(n) {
+  let num = n;
+  let result = 0;
 
-    for(let i = 0; i < String(n).length; i++){
-       result += num % 10;
-       num = Math.floor(num/10);        
-    }
-    return n + result;
+  for (let i = 0; i < String(n).length; i++) {
+    result += num % 10;
+    num = Math.floor(num / 10);
+  }
+  return n + result;
 }
 
-let selfNumArr = new Array(n+1).fill(true);
+let selfNumArr = new Array(n + 1).fill(true);
 
-for(let i = 0; i <= n; i++){
-    if(d(i) <= n){
-       selfNumArr[d(i)] = false;
-    }
+for (let i = 0; i <= n; i++) {
+  if (d(i) <= n) {
+    selfNumArr[d(i)] = false;
+  }
 }
 
-for(let j =0; j < n; j++){
-    if(selfNumArr[j]){
-        console.log(j);
-    }
+for (let j = 0; j < n; j++) {
+  if (selfNumArr[j]) {
+    console.log(j);
+  }
 }
 
 // https://itsseo.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EB%AC%B8%EC%A0%9C%ED%92%80%EC%9D%B4-javascript-4673%EB%B2%88-%EC%85%80%ED%94%84%EB%84%98%EB%B2%84
---
+++ 백준문제풀기/42.백준1065.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(file).toString();
+
+// 입력값의 범위를 가진 for문이 필요함
+// 각 값들의 차이가 같은 등차수열일 경우 count ++;
+// count를 출력시킨다.
+let count = 0;
+
+for (let i = 1; i <= input; i++) {
+  let numArr = String(i).split("");
+  if (numArr.length < 3) {
+    count++;
+    continue;
+  } else {
+    let distance = numArr[0] - numArr[1];
+    let distance2 = numArr[1] - numArr[2];
+
+    if (distance === distance2) count++;
+  }
+}
+console.log(count);
